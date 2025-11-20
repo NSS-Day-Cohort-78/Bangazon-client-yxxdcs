41de3d52d89eec1b5f24c53a7d1326b3bb473383
charlesmerriman <zacmerr@gmail.com>
2025-11-20 14:28:53 -0600

    Fixed error when adding to cart and completing order

--- data/payment-types.js
+++ data/payment-types.js
@@ -1,29 +1,29 @@
-import { fetchWithResponse, fetchWithoutResponse } from "./fetcher";
+import { fetchWithResponse, fetchWithoutResponse } from "./fetcher"
 
 export function getPaymentTypes() {
-  return fetchWithResponse('payment-types', {
-    headers: {
-      Authorization: `Token ${localStorage.getItem('token')}`
-    }
-  })
+	return fetchWithResponse("paymenttypes", {
+		headers: {
+			Authorization: `Token ${localStorage.getItem("token")}`
+		}
+	})
 }
 
 export function addPaymentType(paymentType) {
-  return fetchWithResponse(`payment-types`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Token ${localStorage.getItem('token')}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(paymentType)
-  })
+	return fetchWithResponse(`paymenttypes`, {
+		method: "POST",
+		headers: {
+			Authorization: `Token ${localStorage.getItem("token")}`,
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(paymentType)
+	})
 }
 
 export function deletePaymentType(id) {
-  return fetchWithoutResponse(`payment-types/${id}`, {
-    method: 'DELETE',
-    headers: {
-      Authorization: `Token ${localStorage.getItem('token')}`
-    }
-  })
+	return fetchWithoutResponse(`paymenttypes/${id}`, {
+		method: "DELETE",
+		headers: {
+			Authorization: `Token ${localStorage.getItem("token")}`
+		}
+	})
 }
