43495ba0a1bca837f446f2235f6956b216454668
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-12-02 15:38:24 -0600

    changed the url for the fetch request for unLikeProduct

--- data/products.js
+++ data/products.js
@@ -114,7 +114,7 @@ export function likeProduct(productId) {
 }
 
 export function unLikeProduct(productId) {
-	return fetchWithoutResponse(`products/${productId}/unlike`, {
+	return fetchWithoutResponse(`products/${productId}/like`, {
 		method: "DELETE",
 		headers: {
 			Authorization: `Token ${localStorage.getItem("token")}`,
